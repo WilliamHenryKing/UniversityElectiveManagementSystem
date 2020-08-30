914ef2d41ecb8837fc098ee973d23bf51ed707e9
William Henry King <kingiamwill@gmail.com>
2020-08-30 23:11:27 +0200

    Added Impl and Impl tests of Lecturer and Gender and modified some Gender.java code.

--- src/main/java/za/ac/cput/elective/repository/IRepository.java
+++ src/main/java/za/ac/cput/elective/repository/IRepository.java
@@ -1,5 +1,11 @@
 package za.ac.cput.elective.repository;
 
+/**
+ * @author @WilliamHenryKing
+ * Desc: General Interface for Repository
+ * Date: 2020/08/26
+ **/
+
 public interface IRepository<T, ID> {
 
     T create(T t);
