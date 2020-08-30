e1ce3913c7ec441822ea5ee13d6848252718ad3d
wasiemcassiem <git config --global user.email ^C>
2020-08-30 14:25:16 +0200

    Created MarkRepImpl and Mark Repo and then ran multiple tests.

--- src/main/java/za/ac/cput/elective/factory/MarkFactory.java
+++ src/main/java/za/ac/cput/elective/factory/MarkFactory.java
@@ -7,11 +7,13 @@ package za.ac.cput.elective.factory;
  */
 
 import za.ac.cput.elective.entity.Mark;
+import za.ac.cput.elective.service.IDGenerator;
 
 public class MarkFactory
 {
     public  static Mark createMark (String marksID)
     {
+        //String marksID = IDGenerator.generateId();
         Mark mark = new Mark.Builder().setMarksID(marksID).build();
         return mark;
     }
---
+++ src/test/java/za/ac/cput/elective/repository/impl/MarkRepositoryImplTest.java
@@ -0,0 +1,58 @@
+package za.ac.cput.elective.repository.impl;
+
+/**
+ * @author @Wasiem Cassiem
+ * Desc: Testing the mark repository here
+ * Date: 2020/08/29
+ **/
+
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import za.ac.cput.elective.entity.Mark;
+import za.ac.cput.elective.factory.MarkFactory;
+import za.ac.cput.elective.repository.MarkRepository;
+
+import static org.junit.Assert.*;
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
+public class MarkRepositoryImplTest
+{
+    private static MarkRepository repository = MarkRepositoryImpl.getRepository();
+    private static Mark mark = MarkFactory.createMark("001");
+    @Test
+    public void getRepository() {
+    }
+
+    @Test
+    public void a_create() {
+        Mark created = repository.create(mark);
+        Assert.assertEquals(mark.getMarksID(), created.getMarksID());
+        System.out.println("Created: " + created);
+    }
+
+    @Test
+    public void b_read() {
+        Mark read = repository.read(mark.getMarksID());
+        System.out.println("Read: " + read);
+    }
+
+    @Test
+    public void c_update() {
+        Mark updated = new Mark.Builder().copy(mark).setMarksID("001").build();
+        updated = repository.update(updated);
+        System.out.println("Updated: " + updated);
+    }
+
+    @Test
+    public void e_delete() {
+        boolean deleted = repository.delete(mark.getMarksID());
+        Assert.assertTrue(deleted);
+    }
+
+    @Test
+    public void m_getAll() {
+        System.out.println("Get all: " + repository.getAll());
+    }
+}
