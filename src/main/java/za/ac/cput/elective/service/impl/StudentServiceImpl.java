886ac49c968cf61abce06943614ecb98d1b012a9
Dogfoger <39027628+Dogfoger@users.noreply.github.com>
2020-10-27 13:49:31 +0200

    Added DB stuff to Student

--- src/main/java/za/ac/cput/elective/service/impl/StudentServiceImpl.java
+++ src/main/java/za/ac/cput/elective/service/impl/StudentServiceImpl.java
@@ -1,11 +1,12 @@
 package za.ac.cput.elective.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import za.ac.cput.elective.entity.Student;
 import za.ac.cput.elective.repository.StudentRepository;
-import za.ac.cput.elective.repository.impl.StudentRepositoryImpl;
 import za.ac.cput.elective.service.StudentService;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author @Dogfoger / Sean Trainor 218060033
@@ -17,8 +18,9 @@ import java.util.Set;
 public class StudentServiceImpl implements StudentService {
     
     private static StudentService s = null;
+    @Autowired
     private StudentRepository st;
-
+/*
     private StudentServiceImpl() {
         this.st = StudentRepositoryImpl.getStudentRepository();
     }
@@ -29,31 +31,41 @@ public class StudentServiceImpl implements StudentService {
         }
         return s;
     }
-
+*/
     @Override
     public Set<Student> getAll() {
-        return this.st.getAll();
+        return this.st.findAll().stream().collect(Collectors.toSet());
     }
 
     @Override
     public Student create(Student student) {
 
-        return this.st.create(student);
+        return this.st.save(student);
 
     }
 
     @Override
     public Student read(String s) {
-        return this.st.read(s);
+        return this.st.findById(s).orElseGet(null);
     }
 
     @Override
     public Student update(Student student) {
-        return this.st.update(student);
+        if(this.st.existsById(student.getStudentID())) {
+            return this.st.save(student);
+        }
+        return null;
     }
 
     @Override
     public boolean delete(String s) {
-        return this.st.delete(s);
+        this.st.deleteById(s);
+        if (this.st.existsById(s)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+
     }
 }
