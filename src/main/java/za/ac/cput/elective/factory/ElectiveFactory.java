b0eff8611d301287b1287240a16b85418e9685ff
Shane Mapasie <60961405+SPMapasie216182425@users.noreply.github.com>
2020-11-26 15:18:29 +0200

    UPDATED

--- src/main/java/za/ac/cput/elective/factory/ElectiveFactory.java
+++ src/main/java/za/ac/cput/elective/factory/ElectiveFactory.java
@@ -1,14 +1,16 @@
 package za.ac.cput.elective.factory;
 
 import za.ac.cput.elective.entity.Elective;
+import za.ac.cput.elective.service.IDGenerator;
 
 import java.util.*;
 public class ElectiveFactory {
 
 
-    public static Elective createElective(long electCode, String electName) {
+    public static Elective createElective(String electCode, String electName) {
         Elective elect = new Elective.Builder()
                 .setElectCode(electCode)
+                //.setElectCode(IDGenerator.generateId())
                 .setElectName(electName)
                 .build();
         return elect;
--- src/main/java/za/ac/cput/elective/service/impl/ElectiveServiceImpl.java
+++ src/main/java/za/ac/cput/elective/service/impl/ElectiveServiceImpl.java
@@ -37,8 +37,8 @@ public class ElectiveServiceImpl implements ElectiveService {
 
     }
 
-    @Override
-    public Elective read(Long s) {
+   @Override
+    public Elective read(String s) {
         return this.eRepo.findById(s).orElseGet(null);
     }
 
@@ -51,7 +51,7 @@ public class ElectiveServiceImpl implements ElectiveService {
     }
 
     @Override
-    public boolean delete(Long s) {
+    public boolean delete(String s) {
         this.eRepo.deleteById(s);
         return !this.eRepo.existsById(s);
     }
