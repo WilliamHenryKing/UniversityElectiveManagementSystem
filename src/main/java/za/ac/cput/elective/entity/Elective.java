8aabd770490777a4c8c91268325390330804d6f6
Shane Mapasie <60961405+SPMapasie216182425@users.noreply.github.com>
2020-11-27 22:06:29 +0200

    tbh ion know if this is even correct!!! So whoever can fix it I won't mind.

--- src/main/java/za/ac/cput/elective/entity/Elective.java
+++ src/main/java/za/ac/cput/elective/entity/Elective.java
@@ -5,23 +5,23 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /*
  * @author: Shane  Mapasie, GitHub: SPMapasie216182425
  * Desc: Create Elective.java class with attributes using the Builder pattern
  * Date: 4rd July 2020
  */
-
-
 @Entity
 public class Elective  {
 
-    /* Encapsulation */
-    //@GeneratedValue(generator = "uuid2")
-    //@GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Id// adding the
+    @Id
     private String electCode;
     private String electName;
+   // ArrayList<String> electives = new ArrayList<>();
+
 
     /*No argument constructor, protected*/
     protected Elective(){}
@@ -30,7 +30,6 @@ public class Elective  {
     public Elective(Builder builder) {
         this.electName = builder.electName;
         this.electCode = builder.electCode;
-
     }
 
     //getters
@@ -41,7 +40,6 @@ public class Elective  {
     public String getElectCode() {
         return electCode;
     }
-
 
     //toString Method
     @Override
@@ -56,6 +54,7 @@ public class Elective  {
 
         private String electName;
         private String electCode;
+        private ArrayList<String> electives = new ArrayList<>();
 
         /* setters using builder */
         public Builder setElectName(String electName) {
@@ -67,11 +66,23 @@ public class Elective  {
             this.electCode = electCode;
             return this;
         }
+        public Builder setElectives(ArrayList<String> electives) {
+            this.electives = electives;
+            return this;
+        }//ArrayList set method
+
+        public Builder getElectives() {
+            this.electives = electives;
+            return this;
+        }//ArrayList get method
 
         /*Constructor with Builder Implemented*/
         public Builder copy(Elective elect) {
             this.electName = elect.electName;
             this.electCode = elect.electCode;
+            for ( String e : electives){
+                this.electives.add(e);
+            }
             return this;
         }
 
@@ -80,7 +91,35 @@ public class Elective  {
             return new Elective(this);
         }
 
+    }//end of builder class
+    public static void main(String[] args) {
+
+        ArrayList<String> electives = new ArrayList<>();
+        electives.add("BNA");
+        electives.add("Scala");
+        electives.add("JavaScript");
+        electives.add("Data Structures");
+        electives.add("Android");
+        electives.add("Docker");
+        electives.add("eCommerce");
+        electives.add("Python");
+        electives.add("PHP");
+        electives.add("R Programming");
+        electives.add("C++");
+        electives.add("Ruby");
+        electives.add("SQL");
+        electives.add("HTML");
+        electives.add("Java");
+
+        // Displaying elements
+        System.out.println("ArrayList after add operation:");
+        for(String ignored :electives)
+            System.out.println(electives);
+            Collections.sort(electives);
     }
 }
+
+
+
 
 
